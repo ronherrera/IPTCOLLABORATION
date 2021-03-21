12e63f56bf15668ecc2f4545d1ac4e740995f29b
manu809 <80087325+manu809@users.noreply.github.com>
2021-03-21 20:58:46 +0800

    Create CaseStudy.java

---
+++ CaseStudy.java
@@ -0,0 +1,28 @@
+package nameage;
+
+public class Nameage {
+
+   
+
+         static void Names(String Name) {
+        System.out.println (Name + "/21 years of age ");
+}
+        static void Address(String Address) {
+        System.out.println (Address);
+} 
+       static void Year(String Year) {
+        System.out.println (Year);
+}  
+       public static void main (String[] args) {
+       Names ("Emmanuel Tolentino/September 1");  
+       Names ("Carl Emmanuel Hipolito/December 24");     
+       Names ("Ron Hendrick Herrera/July 24");     
+       System.out.println ();
+       Address ("Their Addresses");
+       Address ("Filomena,San rafael,Tarlac");  
+       Address ("Talaga,Capas,Tarlac");     
+       Address ("Cristo Rey ,Capas,Tarlac");    
+       Year ("3rd year college");
+       Year ("From Tarlac State University");
+}
+}
